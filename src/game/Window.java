1bdd48f093dac78ec6d9921113c4d23c28b0fec4
t1po2 <nv_hung05@yahoo.com>
2026-05-08 20:57:59 +0200

    1

---
+++ src/game/Window.java
@@ -0,0 +1,12 @@
+package game;
+import javax.swing.*;
+
+public class Window {
+
+        public Window(){
+        JFrame frame = new JFrame("No Libraries Needed!");
+        frame.setSize(800,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
