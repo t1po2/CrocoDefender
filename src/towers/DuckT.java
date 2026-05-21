6fc256452a889839e6828bf79978641a8b4c2473
t1po2 <244026076+t1po2@users.noreply.github.com>
2026-05-21 23:49:56 +0200

    reset

--- src/towers/DuckT.java
+++ src/towers/DuckT.java
@@ -8,18 +8,18 @@ public class DuckT extends Tower {
         this.upgrade1Cost = 800;
         this.upgrade2Cost = 1500;
         this.upgrade3Cost = 3000;
-        this.upgrade4Cost = 5000;
+        this.upgrade4Cost = 4000;
 
-        this.upgrade1Desc = "+2 dmg";
+        this.upgrade1Desc = "+1 dmg";
         this.upgrade2Desc = "+20% fireRate";
         this.upgrade3Desc = "+10% range";
-        this.upgrade4Desc = "comming soon...";
+        this.upgrade4Desc = "Enable Splitter Projectiles";
     }
 
 
     @Override
     public void applyUpgrade1(){
-        this.damage +=2;
+        this.damage +=1;
     }
     @Override
     public void applyUpgrade2(){
@@ -31,6 +31,8 @@ public class DuckT extends Tower {
     }
     @Override
     public void applyUpgrade4(){
+        this.projectileKey="splitter_proj";
+        this.lock4 = 4;
     }
     
 }
